f9fa9725f70c16b58741a59b2b0d73bad40fb823
Jan Jorgensen <jandjorgensen@gmail.com>
2018-03-25 00:04:26 -0700

    actually use webpack library guide

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
+const path = require('path');
+
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'can-fixture-defaults.js',
+    library: 'mockData',
+    libraryTarget: 'umd'
+  },
   module: {
     rules: [
       {
